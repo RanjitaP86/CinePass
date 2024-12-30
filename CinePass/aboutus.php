c78625f2126f36856135d12e52d5e18b139a0419
RanjitaP86 <ranjitapatgar86@gmail.com>
2024-12-30 17:24:52 +0530

    Add files via upload

---
+++ CinePass/aboutus.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<body style="font-family: 'Jost', sans-serif;">
+<?php include('header.php'); ?>
+
+    <title>About Us</title>
+    <link rel="stylesheet" type="text/css" href="css/aboutus.css">
+
+    <div class="au-header text-center" style="padding-bottom: 50px; padding-top:50px;font-family: 'Jost', sans-serif; ">
+                <h2 style="text-align: center; font-size: 70px; color: white;">About Us</h2>
+                <medium style="color: black;">Teamwork begins by building trust and the only way to do that is by overcoming our invulnerabilities.</medium>
+                
+    </div>
+ 
+    
+    <div class="container" style="padding-top: 50px; padding-bottom:100px;">
+        
+        <div class="row">
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-inner">
+                        <img src="images/itisha.jpg">
+                        <h4>Ranjita Patgar</h4>
+                        <h6>Developer</h6>
+                        <h6>ranjitapatgar86@gmail.com</h6>
+                    </div>
+                
+                </div>
+            
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-inner">
+                        <img src="images/shreyaa.jpg">
+                        <h4>Smrathi Naik</h4>
+                        <h6>Developer</h6>
+                        <h6>smrathi22@gmail.com</h6>
+                    </div>
+
+                </div>
+            
+            </div>
+            
+                
+</div>
+            </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+<?php include('footer.php'); ?>
+</html>
